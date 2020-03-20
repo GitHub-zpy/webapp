4cc8fdabda359e80de4a076990a4c74911208336
GitHub-zpyy <1771799489@qq.com>
2020-03-20 11:19:12 +0800

    电子杂志测试

---
+++ dzzz/data/js/lang.js
@@ -0,0 +1,569 @@
+var flbooklang = {
+Confirm:[
+'确定',
+'確定',
+'OK'
+],
+Catalog:[
+'目录',
+'目錄',
+'Catalog'
+],
+Homepage:[
+'主页',
+'主頁',
+'Homepage'
+],
+Bookshelf:[
+'书架',
+'書架',
+'Bookshelf'
+],
+Search:[
+'搜索',
+'搜索',
+'Search'
+],
+Findnum:[
+'找到页数',
+'找到頁數',
+'Find the page number'
+],
+Download:[
+'下载',
+'下載',
+'Download'
+],
+Share:[
+'分享',
+'分享',
+'Share'
+],
+Language:[
+'语言',
+'語言',
+'Language'
+],
+ZoomIn:[
+'页面放大',
+'頁面放大',
+'Zoom In'
+],
+ZoomOut:[
+'页面缩小',
+'頁面縮小',
+'Zoom Out'
+],
+PageRestore:[
+'页面还原',
+'頁面還原',
+'Page Restore'
+],
+PreviousPage:[
+'上一页',
+'上一頁',
+'Previous Page'
+],
+NextPage:[
+'下一页',
+'下一頁',
+'Next Page'
+],
+MoreFeatures:[
+'更多功能',
+'更多功能',
+'More Features'
+],
+FullScreen:[
+'全屏',
+'全屏',
+'Full Screen'
+],
+ExitFullScreen:[
+'退出全屏',
+'退出全屏',
+'Exit Full Screen'
+],
+FullScreenOrExitFullScreen:[
+'全屏/退出全屏',
+'全屏/退出全屏',
+'Full Screen Or Exit Full Screen'
+],
+OpenSound:[
+'打开声音',
+'打開聲音',
+'Open Sound'
+],
+TurnOffSound:[
+'关闭声音',
+'關閉聲音',
+'Turn Off Sound'
+],
+TurnOnOrOffTheSound:[
+'打开/关闭声音',
+'打開/關閉聲音',
+'Turn On Or Off The Sound'
+],
+Help:[
+'帮助',
+'幫助',
+'Help'
+],
+PageZoomOrRestore:[
+'页面放大/还原',
+'頁面放大/還原',
+'Page Zoom Or Restore'
+],
+Scan:[
+'扫一扫，手机阅读',
+'掃一掃，手機閱讀',
+'Scan, Mobile Reading'
+],
+DirectoryColumn:[
+'目录栏',
+'目錄欄',
+'Directory Column'
+],
+FirstPage:[
+'第一页',
+'第一頁',
+'First Page'
+],
+CurrentAndTotalNumber:[
+'当前页码/总页数',
+'當前頁碼/總頁數',
+'Current And Total Number'
+],
+LastPage:[
+'最后一页',
+'最後一頁',
+'Last Page'
+],
+AutoFlip:[
+'自动翻页',
+'自動翻頁',
+'Auto Flip'
+],
+ShareBook:[
+'分享书刊',
+'分享書刊',
+'Share Book'
+],
+Toyourfriends:[
+'给您的朋友阅读！',
+'給您的朋友閱讀！',
+'Read To Your Friends!'
+],
+QRCode:[
+'二维码',
+'二維碼',
+'QR Code'
+],
+ShareLinks:[
+'分享链接',
+'分享鏈接',
+'Share Links'
+],
+CopyLink:[
+'复制链接',
+'複製鏈接',
+'Copy Link'
+],
+ShareTo:[
+'分享到',
+'分享到',
+'Share'
+],
+More:[
+'更多',
+'更多',
+'More'
+],
+Cancel:[
+'取消',
+'取消',
+'Cancel'
+],
+Account:[
+'账号',
+'賬號',
+'Account'
+],
+Password:[
+'密码',
+'密碼',
+'Password'
+],
+Login:[
+'登录',
+'登錄',
+'Login'
+],
+Prompt:[
+'提示信息：',
+'提示信息',
+'Prompt message : '
+],
+UsernameOrPasswordIsWrong:[
+'用户名或密码错误',
+'用戶名或密碼錯誤',
+'User name or password error'
+],
+FreeMake:[
+'免费制作',
+'免費制作',
+'FREE'
+],
+Message:[
+'留言',
+'留言',
+'Message'
+],
+Submit:[
+'提交',
+'提交',
+'Submit'
+],
+Name:[
+'读者昵称',
+'讀者昵稱',
+'Name'
+],
+Phone:[
+'手机号码',
+'手機號碼',
+'Phone'
+],
+MailPhone:[
+'邮箱/手机',
+'郵箱/手機',
+'Email/Phone'
+],
+Content:[
+'留言内容',
+'留言內容',
+'Content'
+],
+PleaseEnterMessageContent:[
+'请填写留言内容',
+'請填寫留言內容',
+'Please Enter Message Content'
+],
+MessageSubmittedSuccessfully:[
+'留言提交成功，管理员审核通过后显示您的留言。',
+'留言提交成功，管理員審核通過後顯示您的留言。',
+'The message was submitted successfully, and your message was displayed after the administrator passed the review.'
+],
+Return:[
+'返回',
+'返回',
+'Return'
+],
+Longpresstosave:[
+'长按二维码可以保存到本地',
+'長按二維碼可以保存到本地',
+'Long press to save'
+],
+WeChatfriends:[
+'微信好友',
+'微信好友',
+'WeChat friends'
+],
+QQfriends:[
+'QQ好友',
+'QQ好友',
+'QQ friends'
+],
+QQspace:[
+'QQ空间',
+'QQ空間',
+'QQ space'
+],
+SinaWeibo:[
+'新浪微博',
+'新浪微博',
+'Sina Weibo'
+],
+BaiduPostBar:[
+'百度贴吧',
+'百度貼吧',
+'Baidu Post Bar'
+],
+Douban:[
+'豆瓣',
+'豆瓣',
+'Douban'
+],
+LinkedIn:[
+'领英',
+'領英',
+'LinkedIn'
+],
+PTQRcode:[
+'普通码',
+'普通碼',
+'QR code'
+],
+Applets:[
+'小程序',
+'小程序',
+'Applets'
+],
+SharetoQQ:[
+'分享到QQ',
+'分享到QQ',
+'Share to QQ'
+],
+SharetoQQspace:[
+'分享到QQ空间',
+'分享到QQ空間',
+'Share to QQ space'
+],
+sharetoSinaWeibo:[
+'分享到新浪微博',
+'分享到新浪微博',
+'share to Sina Weibo'
+],
+SharetoBaiduPostBar:[
+'分享到百度贴吧',
+'分享到百度貼吧',
+'Share to Baidu Post Bar'
+],
+SharetoDouban:[
+'分享到豆瓣',
+'分享到豆瓣',
+'Share to Douban'
+],
+SharetoLinkedIn:[
+'分享到领英',
+'分享到領英',
+'Share to LinkedIn'
+],
+SharetoEmile:[
+'分享到邮箱',
+'分享到郵箱',
+'Share to Emile'
+],
+CreatePoster:[
+'生成海报',
+'生成海報',
+'Create Poster'
+],
+Copysharelink:[
+'复制分享链接',
+'復制分享鏈接',
+'Copy link'
+],
+Copyembedcode:[
+'复制嵌入代码',
+'復制嵌入代碼',
+'Copy code'
+],
+Successcopy:[
+'复制成功',
+'復制成功',
+'Success'
+],
+CopyFailed:[
+'复制失败',
+'復制失敗',
+'Failed'
+],
+About:[
+'关于',
+'關於',
+'About'
+],
+Phone:[
+'电话',
+'電話',
+'Phone'
+],
+Mobile:[
+'手机',
+'手機',
+'Mobile'
+],
+Email:[
+'邮箱',
+'郵箱',
+'E-mail'
+],
+WeChat:[
+'微信',
+'微信',
+'WeChat'
+],
+QQ:[
+'QQ',
+'QQ',
+'QQ'
+],
+Website:[
+'网址',
+'網址',
+'Website'
+],
+Address:[
+'地址',
+'地址',
+'Address'
+],
+Publisher:[
+'发布人',
+'發布人',
+'Publisher'
+],
+TotalPages:[
+'总页数',
+'總頁數',
+'Total Pages'
+],
+Views:[
+'阅读次数',
+'閱讀次數',
+'Views'
+],
+Copyt:[
+'复制',
+'復制',
+'Copy'
+],
+Success:[
+'成功',
+'成功',
+'Success'
+],
+Failed:[
+'失败',
+'失敗',
+'Failed'
+],
+Dial:[
+'拨号',
+'撥號',
+'Dial'
+],
+Visit:[
+'访问',
+'訪問',
+'Visit'
+],
+ReleaseDate:[
+'发布时间',
+'發布時間',
+'Release Date'
+],
+SubmittedSuccessfully:[
+'恭喜！您填写的信息提交成功！',
+'恭喜！您填寫的信息提交成功！',
+'Congratulations! The information you filled in was submitted successfully!'
+],
+DoNotSubmit:[
+'对不起！请勿重复提交信息！',
+'對不起！請勿重復提交信息！',
+'Sorry! Please do not submit information repeatedly!'
+],
+IsRequired:[
+'为必填项！',
+'為必填項！',
+'Is required!'
+],
+NotMoreThan:[
+'不能超过',
+'不能超過',
+'Not more than '
+],
+Characters:[
+'个字符！',
+'個字符！',
+' characters!'
+],
+DidNotFill:[
+'您未填写任何信息！',
+'您未填寫任何信息！',
+'You did not fill in any information!'
+],
+Thumbnail:[
+'缩略图',
+'縮略圖',
+'Thumbnail'
+],
+SingleandDoublePageSwitching:[
+'单双页切换',
+'單雙頁切換',
+'Single and Double Page Switching'
+],
+BackOff:[
+'后退',
+'後退',
+'Back off'
+],
+Forward:[
+'前进',
+'前進',
+'Forward'
+],
+SinglePageMode:[
+'单页模式',
+'單頁模式',
+'Single page mode'
+],
+DoublePageMode:[
+'双页模式',
+'雙頁模式',
+'Double page mode'
+],
+EmailSharing:[
+'邮件分享',
+'郵件分享',
+'E-mail Sharing'
+],
+EnterEmail:[
+'请输入接收人邮箱号',
+'請輸入接收人郵箱號',
+"Please enter the recipient's e-mail"
+],
+Send:[
+'发送',
+'發送',
+'Send'
+],
+SendOk:[
+'发送成功',
+'發送成功',
+'Send Successfully'
+],
+GeneratePosters:[
+'生成海报',
+'生成海報',
+'Generate Posters'
+],
+EmailAddressCanNotBeEmpty:[
+'邮箱地址不能为空！',
+'郵箱地址不能為空！',
+'Email address can not be empty!'
+],
+EmailAddressFormatIsIncorrect:[
+'邮箱地址格式不正确！',
+'郵箱地址格式不正確！',
+'The Email address format is incorrect!'
+],
+Sending:[
+'正在发送...',
+'正在發送...',
+'Sending...'
+],
+DownloadPic:[
+'下载图片',
+'下載圖片',
+'Download Pic'
+],
+LongPressToSave:[
+'移动端长按保存图片',
+'移動端長按保存圖片',
+'Long press to save the picture'
+]
+
+
+};
