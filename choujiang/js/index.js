a5a0120de89f3e556bbba401ff171822e4cb212d
GitHub-zpyy <1771799489@qq.com>
2020-04-02 15:30:23 +0800

    新增抽奖项目

---
+++ choujiang/js/index.js
@@ -0,0 +1,125 @@
+//抽奖人员名单
+var allPerson = "成明辉;陈绮曼;杜婉君;邝美红;梁玉婵;练金妍;刘绮瑶;栗文秀;马瑞萌;尼佳平台;盘洁华;思创;"+
+"姚永坚;章伶丽;张闵;张永凤;赵浩宇;周子墨;曹月娴;林艳梅;王聪;冯上文;李思梦;韦志刚;温素;刘珊;钱大洁;杨鸿鑫";
+//领导人员名单
+var leaderArr = ["李董"];
+//未中奖人员名单
+var remainPerson = allPerson.toString().split(";");
+//中奖人员名单
+var luckyMan = [];
+var timer;//定时器
+var times = 1;//抽奖次数,如果不是第一次，不加粗显示领导姓名
+$(function () {
+    iconAnimation();
+    //开始抽奖
+    $("#btnStart").on("click", function () {
+        //判断是开始还是结束
+        if ($("#btnStart").text() === "开始") {
+            if (!$("#txtNum").val()) {
+                showDialog("请输入抽取人数");
+                return false;
+            }
+            if ($("#txtNum").val() > 49) {
+                showDialog("一次最多只能输入49人");
+                return false;
+            }
+            if ($("#txtNum").val() > remainPerson.length) {
+                showDialog("当前抽取人数大于业务总人数<br>当前抽取人数：<b>" + $("#txtNum").val() + "</b>人,业务总人数：<b>" + remainPerson.length + "</b>人");
+                return false;
+            }
+            $("#result").fadeOut();
+            //显示动画框，隐藏中奖框
+            $("#luckyDrawing").show().next().addClass("hide");
+            move();
+            $("#btnStart").text("停止");
+            $("#bgLuckyDrawEnd").removeClass("bg");
+        }
+        else {
+            $("#btnStart").text("开始");//设置按钮文本为开始
+            var luckyDrawNum = $("#txtNum").val();
+            startLuckDraw();//抽奖开始
+
+            $("#luckyDrawing").fadeOut();
+            clearInterval(timer);//停止输入框动画展示
+            $("#luckyDrawing").val(luckyMan[luckyMan.length - 1]);//输入框显示最后一个中奖名字
+            $("#result").fadeIn().find("div").removeClass().addClass("p" + luckyDrawNum);//隐藏输入框，显示中奖框
+            $("#bgLuckyDrawEnd").addClass("bg");//添加中奖背景光辉
+            $("#txtNum").attr("placeholder", "输入抽取人数(" + remainPerson.length + ")");
+        }
+    });
+
+    $("#btnReset").on("click", function () {
+        //确认重置对话框
+        var confirmReset = false;
+        showConfirm("确认重置吗？所有已抽取的人会重新回到抽取池！", function () {
+            //熏置未中奖人员名单
+            remainPerson = allPerson.toString().split(";");
+            //中奖人数框置空
+            $("#txtNum").val("").attr("placeholder", "请输入抽取人数");
+            $("#showName").val("");
+            //隐藏中奖名单,然后显示抽奖框
+            $("#result").fadeOut();//.prev().fadeIn()
+            $("#bgLuckyDrawEnd").removeClass("bg");//移除背景光辉
+            times++;
+            console.log(times);
+
+        });
+    });
+});
+
+//抽奖主程序
+function startLuckDraw() {
+    //抽奖人数
+    var luckyDrawNum = $("#txtNum").val();
+    if (luckyDrawNum > remainPerson.length) {
+        alert("抽取人数大于业务总人数！请修改人数。或者点重置开始将新一轮抽奖！");
+        return false;
+    }
+    //随机中奖人
+    var randomPerson = getRandomArrayElements(remainPerson, luckyDrawNum);
+    var tempHtml = "";
+    $.each(randomPerson, function (i, person) {
+        var sizeStyle = "";
+        if (person.length > 3) {
+            sizeStyle = " style=font-size:" + 3 / person.length + "em";
+        }
+//      if (leaderArr.indexOf(person) > -1 && times == 1) {
+//          tempHtml += "<span><span " + sizeStyle + "><b>" + person + "</b></span></span>";
+//      }
+//      else {
+//          tempHtml += "<span><span " + sizeStyle + ">" + person + "</span></span>";
+//      }
+		tempHtml += "<span><span " + sizeStyle + ">" + person + "</span></span>";
+    });
+    $("#result>div").html(tempHtml);
+    //剩余人数剔除已中奖名单
+    remainPerson = remainPerson.delete(randomPerson);
+    //中奖人员
+    luckyMan = luckyMan.concat(randomPerson);
+    //设置抽奖人数框数字为空
+    $("#txtNum").val("");
+}
+
+//跳动的数字
+function move() {
+    var $showName = $("#showName"); //显示内容的input的ID
+    var interTime = 30;//设置间隔时间
+    timer = setInterval(function () {
+        var i = GetRandomNum(0, remainPerson.length);
+        $showName.val(remainPerson[i]);//输入框赋值
+    }, interTime);
+}
+
+//顶上的小图标，随机动画
+function iconAnimation() {
+    var interTime = 200;//设置间隔时间
+    var $icon = $("#iconDiv>span");
+    var arrAnimatoin = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "wobble", "tada"];
+    var timer2 = setInterval(function () {
+        var i = GetRandomNum(0, $icon.length);
+        var j = GetRandomNum(0, arrAnimatoin.length);
+        //console.log("i:" + i + ",j:" + j);
+        $($icon[i]).removeClass().stop().addClass("animated " + arrAnimatoin[j]);//输入框赋值
+    }, interTime);
+
+}
